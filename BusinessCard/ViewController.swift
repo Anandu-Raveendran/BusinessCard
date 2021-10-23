ee47d326ca15722f275ced214089ac3df348e61d
anandu-raveendran <47anandu007@gmail.com>
2021-10-23 13:17:44 -0700

    Initial Commit

---
+++ BusinessCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusinessCard
+//
+//  Created by Nandu on 2021-10-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
